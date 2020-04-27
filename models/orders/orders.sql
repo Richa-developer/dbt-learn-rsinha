8d8e01f3b5330f7b5b6cdf79dae9724ac665a942
Richa Sinha <richa.sinha@datalytyx.com>
2020-04-27 18:54:25 +0000

    apr27_4

--- models/orders/orders.sql
+++ models/orders/orders.sql
@@ -1,1 +1,12 @@
-select * from {{ ref('customers') }}
+with orders as
+(
+    select 
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        _etl_loaded_at
+    from raw.jaffle_shop.orders
+)
+
+select * from orders
